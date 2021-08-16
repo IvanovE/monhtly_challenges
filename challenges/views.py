4afc87c20cff1633edab48ff706d1f48e474acf1
Evgeny <EugeneIvanov12@gmail.com>
2021-08-16 23:13:10 +0300

    urls and views

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
+
+
+def monthly_challenge(request, month):
+    challenge_text = None
+    if month == 'january':
+        challenge_text = "january"
+    elif month == 'february':
+        challenge_text = "february"
+    elif month == "march":
+        challenge_text = "march"
+    else:
+        return HttpResponseNotFound("Really???")
+    return HttpResponse(challenge_text)
