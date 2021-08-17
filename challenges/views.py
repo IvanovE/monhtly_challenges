1affe4cc6ea6ff17d83bedadb983ed2e85fc88f6
Evgeny <EugeneIvanov12@gmail.com>
2021-08-17 18:05:29 +0300

    added HTML

--- challenges/views.py
+++ challenges/views.py
@@ -5,19 +5,33 @@ from django.urls import reverse
 # Create your views here.
 
 monthly_challenges = {
-    "january": "january",
-    "february": "february",
-    "march": "march",
-    "april": "april",
-    "may": "may",
-    "june": "june",
-    "july": "july",
-    "august": "august",
-    "september": "september",
-    "october": "october",
-    "november": "november",
-    "december": "december"
+    "january": "january1",
+    "february": "february1",
+    "march": "march1",
+    "april": "april1",
+    "may": "may1",
+    "june": "june1",
+    "july": "july1",
+    "august": "august1",
+    "september": "september1",
+    "october": "october1",
+    "november": "november1",
+    "december": "december1"
 }
+
+
+def index(request):
+    list_items = ""
+    months = list(monthly_challenges.keys())
+
+    for month in months:
+        capitalized_month = month.capitalize()
+        month_path = reverse("monthly-challenge", args=[month])
+        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
+
+    response_data = f"<ul>{list_items}</ul>"
+
+    return HttpResponse(response_data)
 
 
 def monthly_challenge_by_number(request, month):
