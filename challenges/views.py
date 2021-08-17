8e3150b91c3b0f74a8488b715c89356c9ead6fd9
Evgeny <EugeneIvanov12@gmail.com>
2021-08-17 14:50:17 +0300

    reverse and redirect

--- challenges/views.py
+++ challenges/views.py
@@ -1,17 +1,38 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 
+monthly_challenges = {
+    "january": "january",
+    "february": "february",
+    "march": "march",
+    "april": "april",
+    "may": "may",
+    "june": "june",
+    "july": "july",
+    "august": "august",
+    "september": "september",
+    "october": "october",
+    "november": "november",
+    "december": "december"
+}
 
-def monthly_challenge(request, month):
-    challenge_text = None
-    if month == 'january':
-        challenge_text = "january"
-    elif month == 'february':
-        challenge_text = "february"
-    elif month == "march":
-        challenge_text = "march"
+
+def monthly_challenge_by_number(request, month):
+    if 0 < month < 13:
+        months = list(monthly_challenges.keys())
+        redirect_month = months[month - 1]
+        redirect_url = reverse("monthly-challenge", args=[redirect_month])
+        return HttpResponseRedirect(redirect_url)
     else:
         return HttpResponseNotFound("Really???")
-    return HttpResponse(challenge_text)
+
+
+def monthly_challenge(request, month):
+    try:
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
+        return HttpResponseNotFound("Really???")
